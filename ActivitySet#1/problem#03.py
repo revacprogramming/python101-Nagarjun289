b0df0c6c5e748b98a06f7f7114046a822bc7027e
nagarjun nataraj <94677992+Nagarjun289@users.noreply.github.com>
2022-05-16 07:13:41 +0000

    03.

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,7 +1,15 @@
 # Variables, Expressions & Statements
 
-def earning(hrs,rate)
-  wages=hrs*rate
-def output()
-
-
+def earning(hrs,rate):
+  wage=hrs*rate
+  return wage
+def output(wage):
+  print('Pay:',wage)
+def main():
+  hrs=float(input("enter hours:"))
+  rate=float(input("enter rate"))
+  wage=earning(hrs,rate)
+  output(wage)
+main()
+  
+  
