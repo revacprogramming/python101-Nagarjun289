302035783554ae63c29097ac89f24cd28cebda48
nagarjun nataraj <94677992+Nagarjun289@users.noreply.github.com>
2022-05-19 10:11:48 +0000

    04.

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,10 +1,15 @@
-sh =  float(input("Enter Hours:"))
-sr = float(input("Enter Rate:"))
-
-
-if sh<=40 :
-  
-   print(sh*sr)
-elif sh>40:
-   print(40*sr+(sh-40)*1.5*sr)
-  
+def earning(hrs,rate):
+    wage=hrs*rate
+    return wage
+def output(wage, hrs, rate):
+    if hrs <= 40:
+        print(wage)
+    elif hrs > 40:
+        print(40*rate + (hrs - 40)* 1.5*rate)
+def main():
+    
+    hrs = float(input('Enter hours:'))
+    rate = float(input('enter the rate:'))
+    wage=(earning(hrs,rate))
+    output(wage, hrs, rate)
+main()  
